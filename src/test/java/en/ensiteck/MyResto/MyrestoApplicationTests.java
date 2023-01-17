da7538ec7aab9550e110b835ace794683a0e96bd
perineau <fperineau28@gmail.com>
2023-01-17 09:27:11 +0100

    init repo

---
+++ src/test/java/en/ensiteck/MyResto/MyrestoApplicationTests.java
@@ -0,0 +1,13 @@
+package en.ensiteck.MyResto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
